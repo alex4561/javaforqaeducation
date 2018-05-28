d225b823e5c99532aefbb3a93c6f07a23ae6e5bd
Alexander Bogomolov <alexander.v.bogomolov@gmail.com>
2018-05-28 12:57:03 +0300

    Доавлен класс Point

---
+++ sandbox/src/main/java/ru/javaforqa/myfirstprogram/Point.java
@@ -0,0 +1,11 @@
+package ru.javaforqa.myfirstprogram;
+
+public class Point {
+    int AxisX;
+    int AxisY;
+
+    public static double getDistance(Point AxisX, Point AxisY){
+        return Math.sqrt(Math.pow(Double.parseDouble(String.valueOf(AxisX1-AxisX2)),2)+(Math.pow(Double.parseDouble(String.valueOf(AxisY2-AxisY1)),2)));
+    }
+
+}
