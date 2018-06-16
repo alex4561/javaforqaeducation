72bef6a6173ac4b50922beeef49757c938370b0c
alex4561 <alexander.v.bogomolov@gmail.com>
2018-06-16 14:51:15 +0300

    Решено ДЗ, убраны лекционные примеры.

--- sandbox/src/main/java/ru/javaforqa/myfirstprogram/MyFirstProgram.java
+++ sandbox/src/main/java/ru/javaforqa/myfirstprogram/MyFirstProgram.java
@@ -3,46 +3,25 @@ package ru.javaforqa.myfirstprogram;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		hello("world");
-		hello("user");
+
 		hello("Alexander");
-/*Создается обьект Squre*/
-		Square s = new Square(5);
 
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-/*Создается обьект Rectangle*/
-        Rectangle r = new Rectangle(4,6);
-
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 /*Считаем расстояние между точками*/
-		/*1) задаем значение переменных, означающих координаты точек*/
-		double AxisX1 = 2;
-		double AxisY1 = 4;
-		double AxisX2 = 4;
-		double AxisY2 = 6;
 		/*Выводим результат*/
-		System.out.println("Расстояние между точками равно "+ getDistance(AxisX1,AxisY1,AxisX2,AxisY2));
+		Point p1 = new Point(2,4, 3, 6);
+		/*Point p2 = new Point(3,6);*/
+		/*System.out.println("Расстояние между точками равно "+ getDistance(AxisX1,AxisY1,AxisX2,AxisY2));*/
+		System.out.println("Расстояние между точками равно "+ p1.getDistance());
+   /* System.out.println("Расстояние между точками равно "+ p2.getDistance());*/
+
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 	}
-	/*Функция, считающая расстояния между точками*/
-	public static double getDistance(double AxisX1, double AxisY1, double AxisX2, double AxisY2){
-		return Math.sqrt(Math.pow(Double.parseDouble(String.valueOf(AxisX1-AxisX2)),2)+(Math.pow(Double.parseDouble(String.valueOf(AxisY2-AxisY1)),2)));
-	}
 
+	/*public static double getDistance(Point p1, Point p2){
+		return Math.sqrt(Math.pow(Double.parseDouble(String.valueOf(p1.AxisX-p2.AxisX)),2)+(Math.pow(Double.parseDouble(String.valueOf(p2.AxisY-p1.AxisY)),2)));
+	}*/
 
 }
-
-/*1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
-2. Создать функцию
-public static double distance(Point p1, Point p2)
-которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
-3. Сделать запускаемый класс, то есть содержащий функцию
-public static void main(String[] args) {...}
-и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
-4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры
-sиспользования метода вместо ранее созданной функции.AB = √(xb - xa)2 + (yb - ya)2
-*/
--- sandbox/src/main/java/ru/javaforqa/myfirstprogram/Point.java
+++ sandbox/src/main/java/ru/javaforqa/myfirstprogram/Point.java
@@ -1,9 +1,20 @@
 package ru.javaforqa.myfirstprogram;
 
-public class Point {
-    public double AxisX;
-    public double AxisY;
 
+public class Point {
+    public double AxisX1;
+    public double AxisY1;
+    public double AxisX2;
+    public double AxisY2;
+    public Point (double X1, double Y1, double X2, double Y2) {
+        this.AxisX1 = X1;
+        this.AxisY1 = Y1;
+        this.AxisX2 = X2;
+        this.AxisY2 = Y2;
+    }
+    public double getDistance(){
+        return Math.sqrt(Math.pow(Double.parseDouble(String.valueOf(this.AxisX1-this.AxisX2)),2)+(Math.pow(Double.parseDouble(String.valueOf(this.AxisY2-this.AxisY1)),2)));
+    }
 
 
 }
